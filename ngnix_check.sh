83318db5619addb89287f4945cdbc12b780d2f81
黃柏澄(風雨) <wartw@users.noreply.github.com>
2022-04-28 01:44:20 +0800

    Update ngnix_check.sh

--- ngnix_check.sh
+++ ngnix_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 date=`date +%Y-%m-%d`
 date_time=`date +%Y-%m-%d_%H:%M:%S`
-logs_back=/etc/nginx/nginx.check.logs
+logs_back=/home/check/nginx/nginx.check.logs
 logs_file=$logs_back/$date.nginx.check.logs
-mkdir -p /etc/nginx/nginx.check.logs
+mkdir -p /home/check/nginx/nginx.check.logs
 touch $logs_file
 nginx_port=`netstat -nlt |grep 443 |awk '{print $4}'| cut -d : -f 2`
 echo ""  >> $logs_file
@@ -22,7 +22,7 @@ else
         echo 'Nginx server is stopped!!'  >> $logs_file
         echo "------------------result------------------" >> $logs_file
         echo '------------------start nginx : lnmp ngnix restart------------------' >> $logs_file
-        lnmp nginx restart
+        lnmp nginx restart >> $logs_file
         nginx_start_port=`netstat -nlt |grep 80 |awk '{print $4}'| cut -d : -f 2`
         echo "nginx_start_port:$nginx_start_port" >> $logs_file
         if [ "$nginx_start_port"="80" ];then
