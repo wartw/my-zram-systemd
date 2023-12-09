0b8021e01eec5aab8aaf88824276676ac54815f3
黃柏澄(風雨) <wartw@users.noreply.github.com>
2023-12-09 18:34:16 +0800

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -3,4 +3,4 @@ fallocate -l 1G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
-echo "\n /swapfile swap swap defaults 0 0" >> /etc/fstab
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
